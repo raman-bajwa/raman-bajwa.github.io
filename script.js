a1a74e5e57a066ed2cf9f185516c7fed81ff7610
ramanbajwa197@gmail.com <Ramanbajwa197@gmail.com>
2022-06-09 21:45:10 +0530

    update: color array

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function getRandomColor(colors) {
 }
 
 function setRandomLinkColor() {
-    let colors = ["#24d05a", "#eb4888", "#10a2f5", "#e9bc3f"];
+    let colors = ["#24d05a", "#eb4888", "#10a2f5", "#e9bc3f", "#242F9B", "#FF0000"];
 
     Array.from(document.getElementsByTagName("a")).forEach((e) => {
         // get random color
